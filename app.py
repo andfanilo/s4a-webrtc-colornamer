deeffa9ea34c2667b11cfd6967c8a883e3dc5637
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2021-09-22 18:36:04 +0200

    Display image from video

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from streamlit_webrtc import (
 )
 
 st.title("WebRTC Colornamer")
+image_placeholder = st.empty()
 
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
@@ -14,6 +15,22 @@ RTC_CONFIGURATION = RTCConfiguration(
 
 """ Simple video loopback """
 webrtc_streamer(
-    key="loopback",
+    key="video-colornamer",
+    mode=WebRtcMode.SENDONLY,
     rtc_configuration=RTC_CONFIGURATION,
+    media_stream_constraints={"video": True},
 )
+
+while True:
+    if webrtc_ctx.video_receiver:
+        try:
+            video_frame = webrtc_ctx.video_receiver.get_frame(timeout=1)
+        except queue.Empty:
+            print("Queue is empty. Abort.")
+            break
+
+        img_rgb = video_frame.to_ndarray(format="rgb24")
+        image_placeholder.image(img_rgb)
+    else:
+        print("AudioReciver is not set. Abort.")
+        break
