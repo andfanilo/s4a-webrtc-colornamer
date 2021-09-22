a2e60a1da5ccd77dba45c867a80172dcf3122418
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2021-09-22 18:26:52 +0200

    Init

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+from streamlit_webrtc import (
+    RTCConfiguration,
+    VideoProcessorBase,
+    WebRtcMode,
+    webrtc_streamer,
+)
 
+st.title("WebRTC Colornamer")
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+
+""" Simple video loopback """
+webrtc_streamer(key="loopback")
